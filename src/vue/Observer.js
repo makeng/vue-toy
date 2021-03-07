c4d112e4d7fc0e2e82c82d01d57722c75e9c61d9
马兆铿 <zhaokeng_ma@kingdee.com>
2021-03-07 13:44:13 +0800

    🌟: [render] add Observer

---
+++ src/vue/Observer.js
@@ -0,0 +1,60 @@
+/* ---------------------------------------------------------------------------------------
+* about:观察者类。
+* author:马兆铿（13790371603 810768333@qq.com）
+* date:2021-03-07
+* ---------------------------------------------------------------------------------------- */
+import Dep from './Dep'
+
+/**
+ * 创建 getter/setter
+ * @param data 对象
+ * @param key 属性值
+ * @param val 初始值。定义 getter/setter 必须自定义一个变量用于保存值，否则 get() 找不到值返回
+ * @returns {*}
+ */
+function defineReactive (data, key, val) {
+  // 递归属性，进行观察
+  if (typeof data === 'object') {
+    new Observer(data)
+  }
+
+  // 挂载
+  const dep = new Dep() // 变化收集器
+  Object.defineProperty(data, key, {
+    configurable: true,
+    enumerable: true,
+    set (newVal) {
+      if (newVal === val) {
+        return
+      }
+      val = newVal
+      dep.notify()
+    },
+    get () {
+      dep.depend()
+      return val
+    }
+  })
+}
+
+// 深度遍历，并用于遍历数据并挂载监听。
+class Observer {
+  constructor (value) {
+    this.value = value
+    // 如果是数组，用别的处理方式
+    if (!Array.isArray(value)) {
+      this.walk(value)
+    }
+  }
+
+  // 把每个属性都变成 getter/setter，进行监听。此方法在类型为 Object 时才调用
+  walk (obj) {
+    const keys = Object.keys(obj)
+    for (let i = 0; i < keys.length; i++) { // 深度只有一层
+      const key = keys[i]
+      defineReactive(obj, key, obj[key])
+    }
+  }
+}
+
+export default Observer

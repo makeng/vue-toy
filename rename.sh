b7cb8263779d6e7fa8b0f654096d2a7ef8f5dc9c
makeng <810768333@qq.com>
2021-12-05 22:01:18 +0800

    📁: [sh] add rename bash

---
+++ rename.sh
@@ -0,0 +1,15 @@
+git filter-branch -f --env-filter '
+OLD_EMAIL="zhaokeng_ma@kingdee.com"
+CORRECT_NAME="makeng"
+CORRECT_EMAIL="810768333@qq.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
